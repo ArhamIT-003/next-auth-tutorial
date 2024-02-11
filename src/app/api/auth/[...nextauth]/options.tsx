d08f036d03da62e170efc6723308457d5967bf60
Arham <muhammadarham59@gmail.com>
2024-02-11 16:49:46 +0500

    Credientials provider work also done

--- src/app/api/auth/[...nextauth]/options.tsx
+++ src/app/api/auth/[...nextauth]/options.tsx
@@ -5,6 +5,27 @@ import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
 import { GithubProfile } from "next-auth/providers/github";
 import { GoogleProfile } from "next-auth/providers/google";
+import User from "@/models/User";
+import bcrypt from "bcrypt";
+import { Console } from "console";
+
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+async function login(credentials: Credentials) {
+  const user = await User.findOne({ email: credentials.email });
+  if (user) {
+    const match = await bcrypt.compare(credentials.password, user.password);
+
+    if (match) {
+      console.log("User found");
+      return user; // Returning user for further processing
+    }
+  }
+  return null; // Return null if user not found or password doesn't match
+}
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -44,6 +65,34 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith2company.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "your-password",
+        },
+      },
+      async authorize(credentials?: Credentials) {
+        try {
+          if (!credentials) {
+            throw new Error("Credentials are required");
+          }
+          const user = await login(credentials);
+          console.log(user);
+          return user; // Return user if found, null otherwise
+        } catch (err) {
+          console.log(err);
+          return null;
+        }
+      },
+    }),
   ],
   callbacks: {
     async jwt({ token, user }) {
@@ -58,23 +107,3 @@ export const authOptions: NextAuthOptions = {
 };
 
 export default NextAuth(authOptions);
-
-// CredentialsProvider({
-//   name: "Credentials",
-//   credentials: {
-//     username: { label: "Username", type: "text", placeholder: "jsmith" },
-//     password: { label: "Password", type: "password" },
-//   },
-//   async authorize(credentials) {
-//     const user = { id: "42", name: "Arham", password: "12345667" };
-
-//     if (
-//       credentials?.username === user.name &&
-//       credentials?.password === user.password
-//     ) {
-//       return user;
-//     } else {
-//       return null;
-//     }
-//   },
-// }),
